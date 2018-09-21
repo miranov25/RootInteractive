f2c3e65b42bf30cdb85820179ba580cd03809e70
miranov25 <marian.ivanov@cern.ch>
2018-09-21 13:50:42 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ VERSION = None
 
 # What packages are required for this module to be executed?
 REQUIRED = [
-    # 'requests', 'maya', 'records',
+    # 'requests', 'maya', 'records', 'ipywidgets', 'matplotlib', 'numpy',
 ]
 
 # What packages are optional?
