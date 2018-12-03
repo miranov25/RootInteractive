f0b4a3a5f21cb467ca0ba16e7f5f48a2ec7862fe
miranov25 <marian.ivanov@cern.ch>
2018-12-03 20:39:26 +0100

    PWGPP-485 - update moving declaration of variables

--- TTreeHnInteractive/bokehDrawPanda.py
+++ TTreeHnInteractive/bokehDrawPanda.py
@@ -3,53 +3,72 @@ import re
 from bokeh.models import *
 from bokehTools import *
 from ipywidgets import *
+from functools import partial
+from IPython.display import display
 
 
-class bokehDrawPanda:
+class bokehDrawPanda(object):
+
     def __init__(self, source, query, varX, varY, varColor, sliderString, p, **options):
+        '''
+        :param source:           input dataframe
+        :param query:            query string
+        :param varX:             X variable name
+        :param varY:             : separated list of the Y varaibles
+        :param varColor:         color map variable name
+        :param sliderString:     :  separated sting - list of sliders var(min,max,step, minValue,maxValue)
+        :param p:                template figure
+        :param options:          optional drawing parameters
+                                 - ncols - number fo columns in drawing
+                                 - commonX=?,commonY=? - switch share axis
+        '''
         self.query = query
         self.dataSource = source.query(query)
-        self.sliderWidgets = []
+        self.sliderWidgets = 0
+        self.sliderArray = []
         self.varX = varX
         self.varY = varY
         self.varColor = varColor
         self.options = options
         self.initSliders(sliderString)
-        print(options)
         self.figure, self.handle, self.bokehSource = drawColzArray(source, query, varX, varY, varColor, p, **options)
-        print(options)
+        display(self.sliderWidgets)
 
     def initSliders(self, sliderString):
         """
         parse sliderString string and create range sliders
         :param sliderString:   example string - name0(min,max,step,valMin,valMax): ....
-        :return: srt sliders
+        :return: s sliders
         """
-        self.sliderWidgets = []
+        self.sliderArray = []
         sliderList0 = sliderString.split(":")
         for i, slider in enumerate(sliderList0):
             values = re.split('[(,)]', slider)
-            slider = RangeSlider(start=float(values[1]), end=float(values[2]), step=float(values[3]), value=(float(values[4]), float(values[5])), title=values[0])
-            #interact(self.updateInteractive(), slider)
-            self.sliderWidgets.append(slider)
-        show(widgetbox(self.sliderWidgets))
+            # slider = RangeSlider(start=float(values[1]), end=float(values[2]), step=float(values[3]), value=(float(values[4]), float(values[5])), title=values[0])
+            slider = widgets.FloatRangeSlider(description=values[0], layout=Layout(width='66%'), min=float(values[1]), max=float(values[2]), step=float(values[3]),
+                                              value=[float(values[4]), float(values[5])])
+            slider.observe(self.updateInteractive, names='value')
+            self.sliderArray.append(slider)
+        self.sliderWidgets = widgets.VBox(self.sliderArray, layout=Layout(width='66%'))
 
-    def updateInteractive(self):
+    def updateInteractive(self, b):
         sliderQuery = ""
-        for slider in self.sliderWidgets:
-            sliderQuery += str(str(slider.title) + ">" + str(slider.value[0]) + "&" + str(slider.title) + "<" + str(slider.value[1]) + "&")
+        for slider in self.sliderArray:
+            sliderQuery += str(str(slider.description) + ">" + str(slider.value[0]) + "&" + str(slider.description) + "<" + str(slider.value[1]) + "&")
         sliderQuery = sliderQuery[:-1]
-        newSource = self.dataSource.query(sliderQuery)
-        self.bokehSource = newSource
+        newSource = ColumnDataSource(self.dataSource.query(sliderQuery))
+        self.bokehSource.data = newSource.data
+        print(sliderQuery)
         push_notebook(self.handle)
 
-    query = 0  # init query
-    dataSource = 0  # original data source
-    bokehSource = 0  # source used for bokeh
-    sliderWidgets = []  # slider widgets to interact with figures
-    handle = 0  # handle to interact with the jupyter
-    figure = 0  # master figure
-    varX = 0  # x axis variable name
-    varY = 0  # y axis variable names
-    varColor = 0  # color variable name
-    options = 0  # drawing options
+    # query = 0  # init query
+    # dataSource = 0  # original data source
+    # bokehSource = 0  # source used for bokeh
+    # sliderWidgets = 0  # slider widgets to interact with figures
+    # sliderArray=[]
+    # handle = 0  # handle to interact with the jupyter
+    # figure = 0  # master figure
+    # varX = 0  # x axis variable name
+    # varY = 0  # y axis variable names
+    # varColor = 0  # color variable name
+    # options = 0  # drawing options
