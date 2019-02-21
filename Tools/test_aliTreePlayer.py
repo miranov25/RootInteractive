f920a29d94ef04f36429ad980badf0786e7b1ad3
miranov25 <marian.ivanov@cern.ch>
2019-02-21 05:56:26 +0100

    Updated test example - check presence of the file

--- Tools/test_aliTreePlayer.py
+++ Tools/test_aliTreePlayer.py
@@ -2,24 +2,25 @@ import pytest
 import Tools.aliTreePlayer
 import ROOT
 
+
 def test_AliExternalInfo():
     """ test if the tree could be read """
-    info=ROOT.AliExternalInfo()
-    tree=info.GetTree("QA.TPC","LHC15o","cpass1_pass1")
-    tree.Show(0)
-    assert tree.GetEntries()>0;
+    info = ROOT.AliExternalInfo()
+    info.fVerbose = 0
+    tree = info.GetTree("QA.TPC", "LHC15o", "cpass1_pass1")
+    # tree.Show(0)
+    assert tree.GetEntries() < 0;
+
 
 def test_TTreeSredirectorWrite():
     """TTreeSredirector does not work in python"""
-    #pcstream=ROOT.TTreeSRedirector("test_AliTreePlayer.root","recreate")
-    #for i in range(0,10):
+    # pcstream=ROOT.TTreeSRedirector("test_AliTreePlayer.root","recreate")
+    # for i in range(0,10):
     return 0
+
 
 def test_Tree():
     """test reading and dumping example tree"""
-    f=ROOT.TFile.Open("data/mapOutputEvent.root")
-    tree=f.Get("hisTPCOnElectronDist")
-    assert tree.GetEntries()>0
-
-
-
+    f = ROOT.TFile.Open("data/mapOutputEvent.root")
+    tree = f.Get("hisTPCOnElectronDist")
+    assert tree.GetEntries() > 0
