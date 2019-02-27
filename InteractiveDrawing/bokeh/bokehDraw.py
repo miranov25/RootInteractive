507145e9eba1c5f14328afc3a781241a2d7dcc56
janthamade <maynak@excite.com>
2019-02-27 12:20:25 +0100

    Shadow variables are cleaned

--- InteractiveDrawing/bokeh/bokehDraw.py
+++ InteractiveDrawing/bokeh/bokehDraw.py
@@ -22,27 +22,27 @@ class bokehDraw(object):
         :param varXerr:          variable name of the errors on X 
         :param varYerr:          : separated list of the errors on Y variables
         :param varColor:         color map variable name
-        :param widgetString:     :  separated string - list of widgets seperated by ','
+        :param widgetString:     :  separated string - list of widgets separated by ','
                                  widget options: dropdown, checkbox, slider
-                                     slider:
-                                         Requires 4 or 5 numbers as parameters
-                                         for single valued sliders: slider.name(min,max,step,initial value)
-                                             Ex: slider.commonF(0,15,5,0)
-                                         for Ranged sliders:   slider.name(min,max,step,initial start value, initial end value)
-                                             Ex: slider.commonF(0,15,5,0,10)
-                                     checkbox:
-                                         Requires 1 or none parameters. Allowed parameters are: 0,1,True,False
-                                         checkbox.name(initial value default=False)
-                                             Ex: checkbox.isMax(True)
-                                     dropdown menu:
-                                         Requires 1 or more parameters.
-                                         dropdown.name(option1,option2,....)
-                                             Ex: dropdown.MB(0,0.5,1)
+                                  slider:
+                                    Requires 4 or 5 numbers as parameters
+                                    for single valued sliders: slider.name(min,max,step,initial value)
+                                      Ex: slider.commonF(0,15,5,0)
+                                    for Ranged sliders: slider.name(min,max,step,initial start value, initial end value)
+                                      Ex: slider.commonF(0,15,5,0,10)
+                                  checkbox:
+                                    Requires 1 or none parameters. Allowed parameters are: 0,1,True,False
+                                    checkbox.name(initial value default=False)
+                                      Ex: checkbox.isMax(True)
+                                  dropdown menu:
+                                    Requires 1 or more parameters.
+                                    dropdown.name(option1,option2,....)
+                                      Ex: dropdown.MB(0,0.5,1)
                                              
                                  to group widget you can use accordion or tab:
-                                     Ex: 
-                                         accordion.group1(widget1,widget2...), accordion.group2(widget1,widget2...)
-                                         tab.group1(widget1,widget2...), tab.group2(widget1,widget2...)
+                                   Ex:
+                                     accordion.group1(widget1,widget2...), accordion.group2(widget1,widget2...)
+                                     tab.group1(widget1,widget2...), tab.group2(widget1,widget2...)
                                          
         :param p:                template figure
         :param options:          optional drawing parameters
@@ -122,11 +122,11 @@ class bokehDraw(object):
             tabBox.append(newBox)
 
         self.accordion = widgets.Accordion(children=accordBox)
-        for i, wdgt in enumerate(self.accordArray):
-            self.accordion.set_title(i, wdgt[0])
+        for i, iWidget in enumerate(self.accordArray):
+            self.accordion.set_title(i, iWidget[0])
         self.tab = widgets.Tab(children=tabBox)
-        for i, wdgt in enumerate(self.tabArray):
-            self.tab.set_title(i, wdgt[0])
+        for i, iWidget in enumerate(self.tabArray):
+            self.tab.set_title(i, iWidget[0])
         self.all = []
         if len(self.widgetArray) != 0:
             self.all += self.widgetArray
@@ -137,43 +137,54 @@ class bokehDraw(object):
 
         self.Widgets = widgets.VBox(self.all, layout=Layout(width='66%'))
 
-    def fillArray(self, widget, array):
+    def fillArray(self, iWidget, array):
         """
         Gets create the specified widget and append it into the given widget array.
-        :param widget:          is a list with 2 entry: 1.entry is a string: "type.name"
+        :param iWidget:          is a list with 2 entry: 1.entry is a string: "type.name"
                                                         2.entry is a list of parameters
         :param array:           is the list of widgets to be added
         """
         global title
-        title = widget[0].split('.')
+        title = iWidget[0].split('.')
         localWidget = 0
         if title[0] == "checkbox":
-            if len(widget[1])==0:
-                value=False
+            if len(iWidget[1]) == 0:
+                status = False
             else:
-                if widget[1][0] in ['True', 'true', '1' ]:
-                    value = True
-                elif widget[1][0] in ['False', 'false', '0']:
-                    value = False
+                if iWidget[1][0] in ['True', 'true', '1']:
+                    status = True
+                elif iWidget[1][0] in ['False', 'false', '0']:
+                    status = False
                 else:
-                    raise ValueError("The parameters for checkbox can only be \"True\", \"False\", \"0\" or \"1\". The parameter for the checkbox {} was:{}".format(title[1],widget[1][0]))
-            localWidget = widgets.Checkbox(description=title[1], layout=Layout(width='66%'), value=value, disabled=False)
+                    raise ValueError("The parameters for checkbox can only be \"True\", \"False\", \"0\" or \"1\". "
+                                     "The parameter for the checkbox {} was:{}".format(title[1], iWidget[1][0]))
+            localWidget = widgets.Checkbox(description=title[1], layout=Layout(width='66%'), value=status,
+                                           disabled=False)
         elif title[0] == "dropdown":
-            values = list(widget[1])
-            if len(values)==0:
-                raise ValueError("dropdown menu requires at least 1 option. The dropdown menu {} has no options", format(title[1]))
-            localWidget = widgets.Dropdown(description=title[1], options=values, layout=Layout(width='66%'), values=values[0])
+            values = list(iWidget[1])
+            if len(values) == 0:
+                raise ValueError("dropdown menu requires at least 1 option. The dropdown menu {} has no options",
+                                 format(title[1]))
+            localWidget = widgets.Dropdown(description=title[1], options=values, layout=Layout(width='66%'),
+                                           values=values[0])
         elif title[0] == "slider":
-            if len(widget[1]) == 4:
-                localWidget = widgets.FloatSlider(description=title[1], layout=Layout(width='66%'), min=float(widget[1][0]), max=float(widget[1][1]), step=float(widget[1][2]), value=float(widget[1][3]))
-            elif len(widget[1]) == 5:
-                localWidget = widgets.FloatRangeSlider(description=title[1], layout=Layout(width='66%'), min=float(widget[1][0]), max=float(widget[1][1]), step=float(widget[1][2]), value=[float(widget[1][3]), float(widget[1][4])])
+            if len(iWidget[1]) == 4:
+                localWidget = widgets.FloatSlider(description=title[1], layout=Layout(width='66%'),
+                                                  min=float(iWidget[1][0]), max=float(iWidget[1][1]),
+                                                  step=float(iWidget[1][2]), value=float(iWidget[1][3]))
+            elif len(iWidget[1]) == 5:
+                localWidget = widgets.FloatRangeSlider(description=title[1], layout=Layout(width='66%'),
+                                                       min=float(iWidget[1][0]), max=float(iWidget[1][1]),
+                                                       step=float(iWidget[1][2]),
+                                                       value=[float(iWidget[1][3]), float(iWidget[1][4])])
             else:
-                raise SyntaxError("The number of parameters for Sliders can be 4 for Single value sliders and 5 for ranged sliders. Slider {} has {} parameters.".format(title[1],len(widget[1])))
+                raise SyntaxError(
+                    "The number of parameters for Sliders can be 4 for Single value sliders and 5 for ranged sliders. "
+                    "Slider {} has {} parameters.".format(title[1], len(iWidget[1])))
         else:
             if (self.verbosity >> 1) & 1:
-                print("type of the widget\""+title[0]+"\" is not specified. Assuming it is a slider.")
-            self.fillArray(["slider."+title[0],widget[1]],array)  #For backward compatibility, it can parse sliders defined as: name(min,max,step,value)
+                print("type of the widget\"" + title[0] + "\" is not specified. Assuming it is a slider.")
+            self.fillArray(["slider." + title[0], iWidget[1]], array)  # For backward compatibility
         if localWidget != 0:
             localWidget.observe(self.updateInteractive, names='value')
             array.append(localWidget)
@@ -181,13 +192,16 @@ class bokehDraw(object):
     def updateInteractive(self, b):
         sliderQuery = ""
         allWidgets = []
-        for widget in [item[1:] for item in self.accordArray] + [item[1:] for item in self.tabArray]: allWidgets += widget
+        for iWidget in [item[1:] for item in self.accordArray] + [item[1:] for item in
+                                                                  self.tabArray]: allWidgets += iWidget
         allWidgets += self.widgetArray
-        for widget in allWidgets:
-            if isinstance(widget, widgets.FloatRangeSlider):
-                sliderQuery += str(str(widget.description) + ">=" + str(widget.value[0]) + "&" + str(widget.description) + "<=" + str(widget.value[1]) + "&")
+        for iWidget in allWidgets:
+            if isinstance(iWidget, widgets.FloatRangeSlider):
+                sliderQuery += str(
+                    "{0}>={1}&{2}<={3}&".format(str(iWidget.description), str(iWidget.value[0]),
+                                                str(iWidget.description), str(iWidget.value[1])))
             else:
-                sliderQuery += str(str(widget.description) + "==" + str(widget.value) + "&")
+                sliderQuery += str(str(iWidget.description) + "==" + str(iWidget.value) + "&")
         sliderQuery = sliderQuery[:-1]
         newSource = ColumnDataSource(self.dataSource.query(sliderQuery))
         self.bokehSource.data = newSource.data
@@ -200,20 +214,20 @@ class bokehDraw(object):
         theContent = pyparsing.Word(pyparsing.alphanums + ".+-") | '#' | pyparsing.Suppress(',') | ':'
         widgetParser = pyparsing.nestedExpr('(', ')', content=theContent)
         widgetList0 = widgetParser.parseString(toParse)[0]
-        for title,widget in izip(*[iter(widgetList0)] * 2):
-            name = title.split('.')
+        for widgetTitle, iWidget in izip(*[iter(widgetList0)] * 2):
+            name = widgetTitle.split('.')
             if name[0] == 'accordion':
                 if findInList(name[1], self.accordArray) == -1:
                     self.accordArray.append([name[1]])
-                for name, param in izip(*[iter(widget)] * 2):
+                for name, param in izip(*[iter(iWidget)] * 2):
                     self.fillArray([name, param], self.accordArray[findInList(name[1], self.accordArray)])
             elif name[0] == 'tab':
                 if findInList(name[1], self.tabArray) == -1:
                     self.tabArray.append([name[1]])
-                for name, param in izip(*[iter(widget)] * 2):
+                for name, param in izip(*[iter(iWidget)] * 2):
                     self.fillArray([name, param], self.tabArray[findInList(name[1], self.tabArray)])
             else:
-                self.fillArray([title, widget], self.widgetArray)
+                self.fillArray([widgetTitle, iWidget], self.widgetArray)
 
     verbosity = 0
 
@@ -231,14 +245,14 @@ def tree2Panda(tree, variables, selection, nEntries, firstEntry, columnMask):
     # replace column names
     #    1.) pandas does not allow dots in names
     #    2.) user can specified own mask
-    for i, column in enumerate(columns):
+    for i, iColumn in enumerate(columns):
         if columnMask == 'default':
-            column = column.replace(".fElements", "").replace(".fX$", "X").replace(".fY$", "Y")
+            iColumn = iColumn.replace(".fElements", "").replace(".fX$", "X").replace(".fY$", "Y")
         else:
             masks = columnMask.split(":")
             for mask in masks:
-                column = column.replace(mask, "")
-        columns[i] = column.replace(".", "_")
+                iColumn = iColumn.replace(mask, "")
+        columns[i] = iColumn.replace(".", "_")
     #    print(i, column)
     # print(columns)
     ex_dict = {}
