0625fb4c7e24847f96f8c6e7c195f447e36a7a65
miranov25 <marian.ivanov@cern.ch>
2019-04-28 07:22:56 +0200

    PWGPP-485, PWGPP-528 - making pip package

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='RootInteractive',
+    version='v0-00-01dev',
+    packages=['RootInteractive',],
+    license='Not define yet. Most probably similar to ALICE (CERN)  license',
+    long_description=open('README.md').read(),
+)
