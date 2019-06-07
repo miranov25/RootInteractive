f17cce386ea93ef0e8d40a8ede14844fba05785d
miranov25 <marian.ivanov@cern.ch>
2019-06-07 06:19:06 +0200

    PWGPP-532 - Test warning supression

--- RootInteractive/Tools/test_aliTreePlayer.py
+++ RootInteractive/Tools/test_aliTreePlayer.py
@@ -57,7 +57,7 @@ def test_TreeParsing():
     widgets = "tab.sliders(slider.meanMIP(45,55,0.1,45,55),slider.meanMIPele(50,80,0.2,50,80), slider.resolutionMIP(0,0.15,0.01,0,0.15)),"
     widgets += "tab.checkboxGlobal(slider.global_Warning(0,1,1,0,1),checkbox.global_Outlier(0)),"
     widgets += "tab.checkboxMIP(slider.MIPquality_Warning(0,1,1,0,1),checkbox.MIPquality_Outlier(0), checkbox.MIPquality_PhysAcc(1))"
-    toRemove = ["^tab\..*"]
+    toRemove = [r"^tab\..*"]
     toReplace = ["^slider.", "^checkbox."]
     print(getAndTestVariableList([selection, varDraw, widgets, "xxx"], toRemove, toReplace, tree))
 
@@ -68,7 +68,7 @@ def test_Parsing():
     tooltips = [('Is maximum', '@isMax'), ("Pad type", "@typeF")]
     slider = "accordion.first(slider.P0(0,1,0.5,0,1),slider.commonF(0,15,5,0,5)),accordion.second(dropdown.MB(0,0.5,1)),accordion.second(checkbox.isMax(False)),slider.typeF(0,4,1,0)"
     varSource = ["x", "varX", "varY", slider, query]
-    toRemove = ["^tab\..*", "^accordion\..*", "False", "True"]
+    toRemove = [r"^tab\..*", r"^accordion\..*", "False", "True"]
     toReplace = ["^slider.", "^checkbox.", "^dropdown."]
     # getAndTestVariableList(varSource,toRemove,toReplace,variables,slider)
     counts = dict()
