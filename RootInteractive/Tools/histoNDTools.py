3c30171288387d5e5a84294fcbf1f298c6399537
miranov25 <marian.ivanov@cern.ch>
2019-06-23 09:05:06 +0200

    PWGPP-517,PWGPP-534 - tools for histogramming and histogram visualization

--- RootInteractive/Tools/histoNDTools.py
+++ RootInteractive/Tools/histoNDTools.py
@@ -3,24 +3,25 @@ import pandas as pd
 import logging
 from bokeh.models import *
 from bokeh.palettes import *
-# from bokeh.models import ColumnDataSource, ColorBar, HoverTool
-# from bokeh.palettes import *
-# from bokeh.transform import *
-from .bokehTools import *
-# from bokeh.layouts import *
+from root_numpy import *
+from RootInteractive.InteractiveDrawing.bokeh.bokehTools import *
 from bokeh.palettes import *
-
 
 # from bokeh.io import push_notebook
 
+# Standard
+histoNDOptions = {
+    "verbose": 0,
+    "colors":  Category10
+}
 
-def makeHistogram(data, hisString, verbose):
+
+def makeHistogram(data, hisString, **kwargs):
     r"""
     Make N dimensional numpy histograms for given histogram string
-
     :param data:            input panda
     :param hisString:       histogram setup string
-    :param verbose:         verbosity flag
+    :param kwargs           options
     :return:
         dictionary with histogram information
             * H              - ndarrray
@@ -31,6 +32,10 @@ def makeHistogram(data, hisString, verbose):
     Example usage:
         >>> makeHistogram(MCdata,"TRD:pmeas:particle:#TRD>0>>hisTRDPP(50,0.5,3,20,0.3,5,5,0,5)",3)
     """
+    options={}
+    options.update(histoNDOptions)
+    options.update(kwargs)
+    verbose=options['verbose']
     if verbose & 0x1:
         logging.info("makeHistogram   :%s", hisString)
     varList = hisString.split(":")
@@ -54,12 +59,35 @@ def makeHistogram(data, hisString, verbose):
         logging.info("Histogram bins  :%s", bins)
         logging.info("Histogram range :%s", hRange)
     H, axis = np.histogramdd(inputArray.values, bins=bins, range=hRange)
-    histo = {"H": H, "axes": axis, "name": histoInfo[0], "varNames": varList}
-    return histo
+    histogram = {"H": H, "axes": axis, "name": histoInfo[0], "varNames": varList}
+    return histogram
+
+def thnToNumpyDD(his):
+    tuple=hist2array(his,False,True,True)
+    histogram={"H":tuple[0],  "axes":tuple[1], "name": his.GetName(), "title":his.GetTitle()}
+    varNames=[]
+    varTitles=[]
+    for axis in range(his.GetNdimensions()):
+        varNames.insert(axis,his.GetAxis(axis).GetName())
+        varTitles.insert(axis,his.GetAxis(axis).GetTitle())
+    histogram["varNames"]=varNames
+    histogram["varTitles"]=varTitles
+    return histogram
 
 
-def makeHistogramPanda(data, hisString, verbose):
-    histo = makeHistogram(data, hisString, verbose)
+def makeHistogramPanda(data, hisString, **kwargs):
+    """
+
+    :param data:
+    :param hisString:
+    :param kwargs:
+    :return:
+    """
+    options={}
+    options.update(histoNDOptions)
+    options.update(kwargs)
+    verbose=options['verbose']
+    histo = makeHistogram(data, hisString, **options)
     varList = histo["varNames"]
     varList.append("count")
     axes = histo["axes"]
@@ -77,8 +105,23 @@ def makeHistogramPanda(data, hisString, verbose):
     df = pd.DataFrame(rows_list, columns=varList)
     return df
 
+def makeHistogramArray(dataSource, histogramStringArray, **kwargs):
+    """
+    :param dataSource:
+    :param histogramStringArray:
+    :return:
+    """
+    options={}
+    options.update(histoNDOptions)
+    options.update(kwargs)
+    histogramArray = []
+    for histoString in histogramStringArray:
+        histogram = makeHistogram(dataSource, histoString,**options)
+        histogramArray.append(histogram)
+    return histogramArray
 
-def bokehDrawHistoSliceColz(histo, hSlice, axisX, axisColor, figOption, graphOption):
+
+def bokehDrawHistoSliceColz(histo, hSlice, axisX, axisColor, axisStep, figOption, graphOption):
     """
     Draw slices of histogram
         prototype usage in https://github.com/miranov25/RootInteractiveTest/blob/394a934ac206da8a7ae8da582fa9e0ae7efd686f/JIRA/PWGPP-517/pandaHistoPrototype.ipynb
@@ -94,47 +137,52 @@ def bokehDrawHistoSliceColz(histo, hSlice, axisX, axisColor, figOption, graphOpt
     :return:                       histogram figure
     """
     #
+    options={}
+    options.update(histoNDOptions)
+    options.update(figOption)
     sliceString = str(hSlice).replace("slice", "")
     TOOLTIPS = [
         ("index", "$index"),
-        ("Slice", sliceString)
+        #("Slice", sliceString)
     ]
     start = hSlice[axisColor].start
     stop = hSlice[axisColor].stop
     step = 1
     hSliceList = list(hSlice)
-    color = Category10[stop - start + 2]
+    maxColor=len(options['colors'])
+    color = options['colors'][min(stop - start + 2,maxColor)]
     data = {}
     x = histo["axes"][axisX][hSlice[axisX]]
     data['varX'] = x
     fIndex = 0
-    for a in xrange(start, stop, step):
+    axis=tuple([a for a in range(0,len(histo["axes"])) if a!=axisX])
+    colorAxisLabel=None
+    try:
+        colorAxisLabel=histo["varTitles"][axisColor]
+    except:
+        colorAxisLabel=histo["varNames"][axisColor]
+    for a in range(start, stop, axisStep):
         hSliceList[axisColor] = slice(a, a + 1, step)
         hSliceLocal = tuple(hSliceList)
         # print a, histo["axes"][axisColor][a]
-        y = np.sum(histo["H"][hSliceLocal], axis=(1, 2))
+        hLocal=histo["H"][hSliceLocal]
+        y = np.sum(histo["H"][hSliceLocal], axis=axis)
         data["varY" + str(fIndex)] = y
-        TOOLTIPS.append(("varY" + str(hSliceList[axisColor]).replace("slice", ""), "@varY" + str(fIndex)))
+        TOOLTIPS.append((colorAxisLabel+"["+str(a)+"]", "@varY" + str(fIndex)))
         fIndex += 1
     source = ColumnDataSource(data)
     p2 = figure(title=histo["name"], tooltips=TOOLTIPS, **figOption)
     fIndex = 0
-    for a in xrange(start, stop, step):
-        p2.scatter("varX", "varY" + str(fIndex), source=source, color=color[fIndex], marker=bokehMarkers[fIndex % 4], legend="Bin" + str(a), **graphOption)
+    for a in range(start, stop, axisStep):
+        xAxisLabel=None
+        try:
+            xAxisLabel=histo["varTitles"][axisX]
+        except:
+            xAxisLabel=histo["varNames"][axisX]
+        p2.scatter("varX", "varY" + str(fIndex), source=source, color=color[fIndex%maxColor],
+                   marker=bokehMarkers[fIndex % 4], legend=colorAxisLabel+"["+str(a)+"]", **graphOption)
+        p2.xaxis.axis_label=xAxisLabel
         fIndex += 1
     p2.legend.click_policy = "hide"
     show(p2)
     return source
-
-
-class bokehDrawHistoND(object):
-    def __init__(self, source, query, varX, varY, varColor, widgetString, p, **options):
-        """
-         :param source:            panda data source  or tree
-         :param query:             selection
-         :param variableList
-         :param widgetString:
-         :param graphOption
-         :param figOption
-         """
-        print(source)
