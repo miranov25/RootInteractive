6a10bb9bb173b00a22cdf58a384532df0eec0362
miranov25 <marian.ivanov@cern.ch>
2019-08-20 05:19:48 +0200

    cleaning

--- test/notebookTest.sh
+++ test/notebookTest.sh
@@ -6,9 +6,3 @@ for note in $(ls */*/*ipynb); do
 done
 
 py.test --nbval  $(ls */*/*ipynb | grep -v aux | grep -v TTreeHn) | tee pytest.log
-
-# py.test --nbval tutorial/bokehDraw/bokehTreeDraw.ipynb    #OK
-# py.test --nbval tutorial/bokehDraw/THnInteractive.ipynb   # test OK - but I had to remove tree.ls()                                                                                                              [ 48%]#
-# py.test --nbval tutorial/bokehDraw/THnVisualization.ipynb .....            #test OK                                                                                                  [ 68%]
-# py.test --nbval tutorial/MLpipeline/plotRegressionForestError.ipynb ....                                                                                                     [ 84%]
-# py.test --nbval tutorial/QuantileRegression/QuantilRegressionForest.ipynb
