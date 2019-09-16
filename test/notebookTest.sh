3fc9c0cbd1db1df515bbeac47921c6a140b11155
miranov25 <marian.ivanov@cern.ch>
2019-09-16 20:50:40 +0200

    Using built-in jupyter to clean notebook

--- test/notebookTest.sh
+++ test/notebookTest.sh
@@ -1,8 +1,9 @@
 #clean_ipynb */*/*ipynb
 for note in $(ls */*/*ipynb); do
     echo clean  ${note}
-    nb-clean clean < ${note}  > ${note}2
-    mv ${note}2 ${note}
+    #nb-clean clean < ${note}  > ${note}2
+    #mv ${note}2 ${note}
+   jupyter nbconvert --ClearOutputPreprocessor.enabled=True --inplace ${note}
 done
 
 py.test --nbval  $(ls */*/*ipynb | grep -v aux | grep -v TTreeHn) | tee pytest.log
