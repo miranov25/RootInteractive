c8ef2e2d271ca2b216ed4ef94b5c50e1e83bcae1
janthamade <maynak@excite.com>
2019-11-09 17:53:50 +0100

    LoadTree implementation draft (Seg.Fault)

--- RootInteractive/Tools/aliTreePlayer.py
+++ RootInteractive/Tools/aliTreePlayer.py
@@ -504,3 +504,76 @@ def GetMetadata(tree, *args):
         nDots = metaName.count('.')
     named = metaData.FindObject(metaName)
     return named
+
+
+def LoadTrees(inputDataList, chRegExp, chNotReg, inputFileSelection, verbose):
+
+    regExp = ROOT.TPRegexp(chRegExp)
+    notReg = ROOT.TPRegexp(chNotReg)
+    regExpArray = inputFileSelection.split(':')
+    residualMapList = ROOT.gSystem.GetFromPipe(inputDataList).Tokenize('\n')
+    nFiles = residualMapList.GetEntries()
+    treeBase = ROOT.TTree()
+    tagValue = {}
+    nSelected = 0
+    for iFile in range(nFiles):
+        tagValue.clear()
+        tagValue["Title"]=""
+        #      read metadata #tag:value information - signed by # at the beginning of the line
+        for jFile in range(iFile, nFiles):                                # get rid of the loop using continue
+            name = residualMapList.At(jFile).GetName()
+            if name[0] is '#':
+                first = name.find(":")
+                tag = name[1:name.find(":")]
+                value = name[name.find(":")+1:len(name)]
+                tagValue[tag] = value
+            else:
+                iFile = jFile
+                break
+        fileName = residualMapList.At(iFile).GetName()
+        isSelected = True
+        #         check if the file was selected
+        if len(regExpArray) > 0:
+            isSelected =False
+            for entry in range(len(regExpArray)):
+                reg = ROOT.TPRegexp(regExpArray[entry])
+                if reg.Match(fileName):
+                    isSelected = True
+        if not isSelected:
+            continue
+#        logging.info("<LoadTrees>: Load file\t%s", fileName)
+        description = ""
+        option=""
+        if "http" in fileName:
+            option="cacheread"
+        finput = ROOT.TFile.Open(fileName, option)
+        if finput is None:
+            raise NameError("<MakeResidualDistortionReport>: Invalid file name {}".format(fileName))
+#            logging.error("<MakeResidualDistortionReport>: Invalid file name %s", fileName)
+            continue
+        keys = finput.GetListOfKeys()
+        isLegend = False
+        for iKey in range(keys.GetEntries()):
+            if regExp.Match(keys.At(iKey).GetName()) is 0:
+                continue      # is selected
+            if notReg.Match(keys.At(iKey).GetName()) is not 0:
+                continue      # is rejected
+            tree = finput.Get(keys.At(iKey).GetName())       # better to use dynamic cast
+            if treeBase.GetEntries() is 0:
+                finput2 = ROOT.TFile.Open(fileName, option)
+                treeBase = finput2.Get(keys.At(iKey).GetName())
+            fileTitle=tagValue["Title"]
+            if len(fileTitle):
+                treeBase.AddFriend(tree, "{}.{}".format(fileTitle, keys.At(iKey).GetName()))
+            else:
+                treeBase.AddFriend(tree, "{}".format(keys.At(iKey).GetName()))
+            entriesF = tree.GetEntries()
+            entriesB = treeBase.GetEntries()
+            if entriesB == entriesF:
+                if (verbose>0):
+                    logging.info("InitMapTree: %s\t%s.%s:\t%d\t%d", treeBase.GetName(), fileName, keys.At(iKey).GetName(), entriesB, entriesF)
+            else:
+                raise ValueError("InitMapTree: {} {} . {}:  {}  {}".format(treeBase.GetName(), fileName, keys.At(iKey).GetName(), entriesB, entriesF))
+#                logging.error("InitMapTree", "%s\t%s.%s:\t%d\t%d", treeBase.GetName(), fileName, keys.At(iKey).GetName(), entriesB, entriesF)
+    return treeBase
+
