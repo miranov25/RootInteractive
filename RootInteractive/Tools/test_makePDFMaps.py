ba1b8edf09e176a46a0bcda583f700cc814de56f
miranov25 <marian.ivanov@cern.ch>
2020-04-24 10:47:04 +0200

    issues/41 - addin granulatiy as for space charge

--- RootInteractive/Tools/test_makePDFMaps.py
+++ RootInteractive/Tools/test_makePDFMaps.py
@@ -6,7 +6,7 @@ from RootInteractive.InteractiveDrawing.bokeh.bokehDrawSA import *
 
 
 def randomData(nPoints=100000):
-    df = pd.DataFrame(np.random.random_sample(size=(nPoints, 4)), columns=list('ABCD'))
+    df = pd.DataFrame(np.random.random_sample(size=(nPoints, 5)), columns=list('ABCDE'))
     df["noise"] = np.random.normal(0, 0.1, nPoints)
     df["csin"] = np.sin(6.28 * df["C"])
     df["valueOrig"] = df["A"] + np.exp(0 * df["B"]) * df["csin"]
@@ -16,9 +16,9 @@ def randomData(nPoints=100000):
 
 def test_makePDF():
     df = randomData(1000000)
-    histo = makeHistogram(df, "value:A:B:C:D:#A>0>>myhisto(40,-20,20,40,0,1,30,0,1,50,0,1,10,0,1)")
+    histo = makeHistogram(df, "value:A:B:C:D:#A>0>>myhisto(40,-2,2,40,0,1,30,0,1,50,0,1,20,0,1)")
 
-    slices = ((0, 40, 1, 0), (10, 30, 1, 4), (10, 24, 5, 3), (1, 49, 3, 1), (0, 10, 3, 0))
+    slices = ((0, 40, 1, 0), (10, 30, 1, 1), (10, 24, 5, 1), (1, 49, 3, 1), (0, 10, 3, 0))
     dimI = 0
     dframe = makePdfMaps(histo, slices, dimI)
     dframe["csin"] = np.sin(6.28 * dframe["CBinCenter"])
@@ -49,4 +49,15 @@ def test_makePDF():
     bokehDrawSA.fromArray(dframe, "ABinNumber>0", figureArray, widgetParams, layout=figureLayoutDesc, tooltips=tooltips,widgetLayout=widgetLayoutDesc)
     #
 
+def DistMap():
+    df = randomData(100000)
+    # histogram binning as for the distortion calibration
+    histo = makeHistogram(df, "value:A:B:C:D:#A>0>>myhisto(200,-2,2,180,0,1,33,0,33,40,0,40,8,0,1)")
+    print(180*33*33*40*8)
+    slices = ((0, 200, 1, 0), (0, 180, 1, 0), (0, 33, 1, 0), (0, 40, 1, 0), (0, 8, 1, 0))
+    dimI = 0
+    dframe = makePdfMaps(histo, slices, dimI)
+
+
 test_makePDF()
+#DistMap()
