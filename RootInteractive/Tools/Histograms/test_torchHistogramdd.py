8fa8ebc47e98e787c6cf056fe7b105ca36bd3821
Marian Ivanov <majo.ivanov@gmail.com>
2020-07-21 10:36:05 +0200

    Added more unit tests

--- RootInteractive/Tools/Histograms/test_torchHistogramdd.py
+++ RootInteractive/Tools/Histograms/test_torchHistogramdd.py
@@ -13,3 +13,23 @@ def test_histogram_empty():
     H,axes = histogramdd_pytorch(sample,bins = [2,3,4,5,6], range=[(1,2),None,None,None,None])
     assert torch.sum(H) == 0, "Sum of empty histogram is not zero"
     assert len(axes) == 5, "Number of axes invalid"
+
+@pytest.mark.parametrize("seed, N, D, bins",[(665162135,100000,5,[9,8,9,10,2]),(665162135,100000,4,[9,2,4,42])])
+def test_histogram_uniform(seed,N,D,bins):
+    torch.random.manual_seed(seed)
+    sample = torch.rand(D,N)
+    H,axes = histogramdd_pytorch(sample,bins)
+    Hnp,npaxes = np.histogramdd(sample.T.cpu().numpy(),bins)
+    assert len(axes) == D, "Number of axes invalid"
+    assert torch.sum(H) <= N, "Sum of histogram too high"
+    for i in range(len(axes)):
+        assert len(axes[i]) == bins[i]+1, "Invslid number of edges"
+
+def test_invalid_range():
+    sample = torch.Tensor([[1,2,0,7],[2,1,4,6],[3,5,1,0]])
+    try:
+        H,axes = histogramdd_pytorch(sample,bins = [3,4,8], range=[(1,2),None,(5,4)])
+        ok = False
+    except ValueError:
+        ok = True
+    assert ok, "Accepted garbage input: invalid range"
