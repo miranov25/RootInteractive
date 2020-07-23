3849d82e20d65e2a026a4ba407a7d215e69e0ba6
Marian Ivanov <majo.ivanov@gmail.com>
2020-07-23 09:51:13 +0200

    Added benchmark code

--- RootInteractive/Tools/Histograms/test_torchHistogramdd.py
+++ RootInteractive/Tools/Histograms/test_torchHistogramdd.py
@@ -26,6 +26,20 @@ def test_histogram_uniform(seed,N,D,bins):
     for i in range(len(axes)):
         assert len(axes[i]) == bins[i]+1, "Invalid number of edges"
 
+@pytest.mark.parametrize("seed, N, D, bins",[(665162135,100000,5,[9,8,9,10,2]),(665162135,100000,4,[9,2,4,42])])
+def test_histogram_uniform_randombins(seed,N,D,bins):
+    torch.random.manual_seed(seed)
+    sample = torch.rand(D,N)
+    nbins = bins
+    bins = [torch.sort(torch.rand(i+1))[0] for i in nbins] #+1 because there's 1 more fence post than the length of the fence
+    H,axes = histogramdd_pytorch(sample,bins)
+    Hnp,npaxes = np.histogramdd(sample.T.cpu().numpy(),bins)
+    assert len(axes) == D, "Number of axes invalid"
+    assert torch.sum(H) <= N, "Sum of histogram too high"
+    assert torch.max(torch.abs(H - Hnp)) <= 1, "Histogram inconsistent with numpy histogram"
+    for i in range(len(axes)):
+        assert len(axes[i]) == nbins[i]+1, "Invalid number of edges"
+
 def test_invalid_range():
     sample = torch.Tensor([[1,2,0,7],[2,1,4,6],[3,5,1,0]])
     try:
@@ -33,7 +47,7 @@ def test_invalid_range():
         ok = False
     except ValueError:
         ok = True
-    assert ok, "Accepted garbage input: invalid range"
+    assert ok, "Accepted invalid input: invalid range"
 
 def test_invalid_bins():
     sample = torch.Tensor([[1,2,0,7],[2,1,4,6],[3,5,1,0]])
@@ -42,4 +56,13 @@ def test_invalid_bins():
         ok = False
     except ValueError:
         ok = True
-    assert ok, "Accepted garbage input: bins must be positive"
+    assert ok, "Accepted invalid input: bins must be positive"
+
+def test_invalid_bins_custombinning_empty_axis():
+    sample = torch.Tensor([[1,2,0,7],[2,1,4,6],[3,5,1,0]])
+    try:
+        H,axes = histogramdd_pytorch(sample,bins = [torch.tensor([1,2,3]),torch.tensor([2,3,4]),torch.tensor([1])])
+        ok = False
+    except ValueError:
+        ok = True
+    assert ok, "Accepted invalid input: each axis must have at least 2 edges for custom binning"
