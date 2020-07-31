bc4542d40c5f7ebca58cc832e919a663b0c28f30
miranov25 <marian.ivanov@cern.ch>
2020-07-31 09:21:09 +0200

    PWGPP-485 - fixing PEP warnngs

--- RootInteractive/InteractiveDrawing/bokeh/test_bokehDrawSA.py
+++ RootInteractive/InteractiveDrawing/bokeh/test_bokehDrawSA.py
@@ -14,10 +14,10 @@ output_file("test_bokehDrawSA.html")
 if "ROOT" in sys.modules:
     TFile.SetCacheFileDir("../../data/")
     tree, treeList, fileList = LoadTrees("echo http://rootinteractive.web.cern.ch/RootInteractive/data/tutorial/bokehDraw/treeABCD.root", ".*", ".*ABCDE.*", ".*", 0)
-    AddMetadata(tree, "A.AxisTitle","A (cm)")
-    AddMetadata(tree, "B.AxisTitle","B (cm/s)")
-    AddMetadata(tree, "C.AxisTitle","B (s)")
-    AddMetadata(tree, "D.AxisTitle","D (a.u.)")
+    AddMetadata(tree, "A.AxisTitle", "A (cm)")
+    AddMetadata(tree, "B.AxisTitle", "B (cm/s)")
+    AddMetadata(tree, "C.AxisTitle", "B (s)")
+    AddMetadata(tree, "D.AxisTitle", "D (a.u.)")
 
 df = pd.DataFrame(np.random.random_sample(size=(4000, 6)), columns=list('ABCDEF'))
 initMetadata(df)
@@ -33,10 +33,11 @@ df.meta.metaData = {'A.AxisTitle': "A (cm)", 'B.AxisTitle': "B (cm/s)", 'C.AxisT
 
 figureArray = [
 #   ['A'], ['C-A'], {"color": "red", "size": 7, "colorZvar":"C", "filter": "A<0.5"}],
-    [['A'], ['C-A'], {"color": "red", "size": 7, "colorZvar":"C"}],
+    [['A'], ['C-A'], {"color": "red", "size": 7, "colorZvar": "C"}],
     [['A'], ['C+A', 'C-A']],
-    [['B'], ['C+B', 'C-B'],{"color": "red", "size": 7, "colorZvar":"C"}],
+    [['B'], ['C+B', 'C-B'], {"color": "red", "size": 7, "colorZvar": "C"}],
     [['D'], ['(A+B+C)*D'], {"size": 10}],
+    [['D'], ['D*10'], {"size": 10, "errY": "D"}],
 ]
 #widgets="slider.A(0,1,0.05,0,1), slider.B(0,1,0.05,0,1), slider.C(0,1,0.01,0.1,1), slider.D(0,1,0.01,0,1), checkbox.Bool(1), multiselect.E(0,1,2,3,4)"
 widgets="slider.A(0,1,0.05,0,1), slider.B(0,1,0.05,0,1), slider.C(0,1,0.01,0.1,1), slider.D(0,1,0.01,0,1), checkbox.Bool(1)"
@@ -45,31 +46,31 @@ tooltips = [("VarA", "(@A)"), ("VarB", "(@B)"), ("VarC", "(@C)"), ("VarD", "(@D)
 
 widgetParams=[
     ['range', ['A']],
-    ['range', ['B',0,1,0.1,0,1]],
-    ['range', ['C'], {'type':'minmax'}],
-    ['range', ['D'], {'type':'sigma', 'bins':10, 'sigma':3}],
-    ['range', ['E'], {'type':'sigmaMed', 'bins':10, 'sigma':3}],
-    ['slider', ['AA'],{'bins':10}],
-    ['multiSelect',["DD",0,1,2,3]],
-    ['select',["CC",0,1,2,3]],
+    ['range', ['B', 0, 1, 0.1, 0, 1]],
+    ['range', ['C'], {'type': 'minmax'}],
+    ['range', ['D'], {'type': 'sigma', 'bins': 10, 'sigma': 3}],
+    ['range', ['E'], {'type': 'sigmaMed', 'bins': 10, 'sigma': 3}],
+    ['slider', ['AA'], {'bins': 10}],
+    ['multiSelect', ["DD", 0, 1, 2, 3]],
+    ['select',["CC", 0, 1, 2, 3]],
     #['slider','F', ['@min()','@max()','@med','@min()','@median()+3*#tlm()']], # to be implmneted
 ]
-widgetLayoutDesc=[ [0,1,2], [3,4,5], [6,7], {'sizing_mode':'scale_width'} ]
+widgetLayoutDesc=[[0, 1, 2], [3, 4, 5], [6, 7], {'sizing_mode': 'scale_width'}]
 
 figureLayoutDesc=[
-    [0,1,2, {'commonX':1,'y_visible':2,'plot_height':300}],
-    [3,{'plot_height':100}],
-    {'plot_height':100,'sizing_mode':'scale_width'}
+    [0, 1, 2, {'commonX': 1, 'y_visible': 2, 'plot_height': 300}],
+    [3, 4, {'plot_height': 100}],
+    {'plot_height': 100, 'sizing_mode': 'scale_width'}
 ]
 
 def testOldInterface():
     output_file("test_bokehDrawSAOldInterface.html")
-    fig=bokehDrawSA(df, "A>0", "A","A:B:C:D","C",widgets,0,tooltips=tooltips, layout=figureLayout)
-    #fig=bokehDrawSA(tree, "A>0", "A","A:B:C:D","C",widgets,0,tooltips=tooltips, layout=figureLayout)
+    fig=bokehDrawSA(df, "A>0", "A", "A:B:C:D", "C", widgets, 0, tooltips=tooltips, layout=figureLayout)
+    #fig=bokehDrawSA(tree, "A>0", "A", "A:B:C:D", "C",widgets,0,tooltips=tooltips, layout=figureLayout)
 
 def testBokehDrawArraySA():
     output_file("test_bokehDrawSAArray.html")
-    fig=bokehDrawSA.fromArray(df, "A>0", figureArray,widgets,tooltips=tooltips, layout=figureLayout)
+    fig=bokehDrawSA.fromArray(df, "A>0", figureArray, widgets, tooltips=tooltips, layout=figureLayout)
 
 def testBokehDrawArrayWidget():
     output_file("test_BokehDrawArrayWidget.html")
