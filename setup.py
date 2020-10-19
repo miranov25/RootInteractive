6b8a8fcfb1a5a05c2eb1ffa2efc0bc4df17ab55f
miranov25 <marian.ivanov@cern.ch>
2020-10-19 09:51:40 +0200

    PWGPP-557 - creating new release v0.00.22

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
     author='Marian Ivanov',
     url='https://github.com/miranov25/RootInteractive',
     name='RootInteractive',
-    version='v0.00.21',
+    version='v0.00.22',
     #packages=setuptools.find_packages(),
     packages=setuptools.find_packages(exclude=["scripts*", "tests*"]),
     license='Not defined yet. Most probably similar to ALICE (CERN)  license',
