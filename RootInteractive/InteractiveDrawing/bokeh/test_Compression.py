d79a5433ad52497560e5311aad911592f5e404c9
miranov25 <marian.ivanov@cern.ch>
2020-12-26 10:40:27 +0100

    #96 - adding visualization to the toy  DCA histogram browsing

--- RootInteractive/InteractiveDrawing/bokeh/test_Compression.py
+++ RootInteractive/InteractiveDrawing/bokeh/test_Compression.py
@@ -1,30 +1,34 @@
 import pandas as pd
 import numpy as np
-from pandas import CategoricalDtype
+# from pandas import CategoricalDtype
 from scipy.stats import entropy
+from RootInteractive.InteractiveDrawing.bokeh.bokehDrawSA import *
+
 
 def estimateEntropy():
-    random = np.random.randn(10000,3)
-    nBins=10
-    H, edges = np.histogramdd(random, bins = (nBins, nBins, nBins), range=[[-6,6], [-6,6], [-6,6]])
-    a=np.empty(H.size)
-    b=np.empty(H.size)
-    c=np.empty(H.size)
+    random = np.random.randn(10000, 3)
+    nBins = 10
+    H, edges = np.histogramdd(random, bins=(nBins, nBins, nBins), range=[[-6, 6], [-6, 6], [-6, 6]])
+    a = np.empty(H.size)
+    b = np.empty(H.size)
+    c = np.empty(H.size)
     for i in range(H.size):
-        a[i]=edges[0][i%nBins]
-        b[i]=edges[1][(i//nBins)%nBins]
-        c[i]=edges[2][i//(nBins*nBins)]
+        a[i] = edges[0][i % nBins]
+        b[i] = edges[1][(i // nBins) % nBins]
+        c[i] = edges[2][i // (nBins * nBins)]
     #    c[i]=edges[2][i%(nBins*nBins)]
-    df=pd.DataFrame(data={'A': a, 'B': b, 'C': c,'H': H.flatten()})
+    dfE = pd.DataFrame(data={'A': a, 'B': b, 'C': c, 'H': H.flatten()})
     # Test - send to cds
-    entropy(df["A"].value_counts(),base=2)
-    entropy(((df[0:-1]-df[1:])["A"])[1:-1].value_counts(),  base=2)
-    #entropy=0
-    entropy(df["B"].value_counts(),base=2)
-    entropy(((df[0:-1]-df[1:])["B"])[1:-1].value_counts(),  base=2)
-    #entropy=0
-    entropy(df["H"].value_counts(),base=2)
-#entropy=3.1
+    entropy(dfE["A"].value_counts(), base=2)
+    entropy(((dfE[0:-1] - dfE[1:])["A"])[1:-1].value_counts(), base=2)
+    # entropy=0
+    entropy(dfE["B"].value_counts(), base=2)
+    entropy(((dfE[0:-1] - dfE[1:])["B"])[1:-1].value_counts(), base=2)
+    # entropy=0
+    entropy(dfE["H"].value_counts(), base=2)
+
+
+# entropy=3.1
 
 # TODO
 #      if (User coding){
@@ -35,32 +39,78 @@ def estimateEntropy():
 #       1. if Unique<<size
 #           1. Entropy for value_counts
 #           2. Entropy for delta.value-counts
-#           3. Use coding with smaller entopy
+#           3. Use coding with smaller entropy
 #   }
 
+
 def simulatePandaDCA():
-    sigma0=0.1
-    sigma1=1
-    entries=1000
+    """
+    simulate DCA 5 D histograms :
+    DCA: qPt,tgl,mdEd,alpha
+    :return:  panda data frame with histogram and some PDF variables
+    """
+    sigma0 = 0.1
+    sigma1 = 1
+    qPtSlope = 0.1
+    tglSlope = 0.1
+    entries = 1000
     # qPt,tgl.mdEdx.alpha, dCA
-    range=([-5,5],[-1,1],[0,1],[0,2*np.pi],[-10*sigma0,10*sigma0])
-    bins=[50,20,20,12,100]
+    range = ([-5, 5], [-1, 1], [0, 1], [0, 2 * np.pi], [-10 * sigma0 - 1, 10 * sigma0 + 1])
+    bins = [50, 20, 20, 12, 100]
     H, edges = np.histogramdd(sample=np.array([[0, 0, 0, 0, 0]]), bins=bins, range=range)
     indexH = np.arange(H.size)
     indexC = np.unravel_index(indexH, bins)
-    qPtCenter = (edges[0][indexC[0]]+edges[0][indexC[0]+1])*.5
-    tgl = edges[1][indexC[1]]
-    mdEdx = edges[2][indexC[2]]
+    qPtCenter = (edges[0][indexC[0]] + edges[0][indexC[0] + 1]) * .5
+    tgl = (edges[1][indexC[1]] + edges[1][indexC[1] + 1]) * .5
+    mdEdx = (edges[2][indexC[2]] + edges[2][indexC[2] + 1]) * .5
+    alpha = (edges[3][indexC[3]] + edges[3][indexC[3] + 1]) * .5
+    #
+    valueMean = qPtCenter * qPtSlope + tgl * tglSlope
     value = edges[4][indexC[4]]
     valueSigma = sigma0 * np.sqrt(1 + sigma1 * mdEdx * qPtCenter * qPtCenter)
-    weight = np.exp(-value * value / (2 * valueSigma * valueSigma))
+    weight = np.exp(-(value - valueMean) ** 2 / (2 * valueSigma * valueSigma))
     weightPoisson = np.random.poisson(weight * entries, H.size)
     H = weightPoisson
-    df = pd.DataFrame({"qPtCenter": qPtCenter, "tglCenter": tgl, "mdEdxCenter": mdEdx, "weight": H})
-    # to export  panda, columns
-    #       qPtCenter,tglCenter,mdEdxCenter,alphaCenter,
-    #       DCA mean, DCA rms
-    #       DCA, w
-    return H, edges, df
+    df = pd.DataFrame({"qPtCenter": qPtCenter, "tglCenter": tgl, "mdEdxCenter": mdEdx, "V": value, "alphaCenter": alpha,
+                       "mean": valueMean, "rms": valueSigma, "weight": H})
+    return df
 
-H,edges, df = simulatePandaDCA()
+
+def histogramDCAPlot(df):
+    """
+    :param df:
+    :return:
+    """
+    df = simulatePandaDCA()
+    output_file("dcaCompressionDemo.html")
+    optionsAll = {"colorZvar": "mdEdxCenter"}
+    df["Vcorr"] = df["V"] - df["mean"]
+    figureArray = [
+        [['qPtCenter'], ['mean'], optionsAll],
+        [['tglCenter'], ['mean'], optionsAll],
+        [['qPtCenter'], ['tglCenter'], {"color": "red", "size": 2, "colorZvar": "mean", "varZ": "mean"}],
+        [['V'], ['weight'], optionsAll],
+        [['V'], ['histo'], {"range_min": -2, "range_max": 2, "weights": "weight", "nbins": 100}],
+        [['Vcorr'], ['histo'], {"range_min": -2, "range_max": 2, "weights": "weight", "nbins": 100}],
+    ]
+
+    widgetParams = [
+        ['range', ['tglCenter']],
+        ['range', ['qPtCenter']],
+        ['range', ['mdEdxCenter']],
+        ['range', ['alphaCenter']],
+    ]
+    tooltips = [("qPtMean", "@qPtMean")]
+    widgetLayoutDesc = [[0, 1], [2, 3], {'sizing_mode': 'scale_width'}]
+    figureLayoutDesc = [
+        [0, 1, {'plot_height': 150, 'x_visible': 1}],
+        [2, 3, {'plot_height': 150, 'x_visible': 1}],
+        [4, 5, {'plot_height': 150, 'x_visible': 1}],
+        {'plot_height': 250, 'sizing_mode': 'scale_width', "legend_visible": True}
+    ]
+    fig = bokehDrawSA.fromArray(df.sample(100000), "rms>0", figureArray, widgetParams, layout=figureLayoutDesc,
+                                tooltips=tooltips, sizing_mode='scale_width', widgetLayout=widgetLayoutDesc,
+                                nPointRender=10000, rescaleColorMapper=True)
+
+df = simulatePandaDCA()
+histogramDCAPlot(df)
