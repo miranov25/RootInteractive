047bb1bedef0cd6986843feb486c1724bbad21da
Marian Ivanov <majo.ivanov@gmail.com>
2021-11-08 16:26:09 +0100

    Added a bokeh extension that copies a CDS and applies transforms to its columns

---
+++ RootInteractive/InteractiveDrawing/bokeh/CDSAlias.py
@@ -0,0 +1,17 @@
+from bokeh.core.properties import Instance, String, Any, Dict
+from bokeh.models import ColumnarDataSource
+
+
+class CDSAlias(ColumnarDataSource):
+    __implementation__ = "CDSAlias.ts"
+
+    # Below are all the "properties" for this model. Bokeh properties are
+    # class attributes that define the fields (and their types) that can be
+    # communicated automatically between Python and the browser. Properties
+    # also support type validation. More information about properties in
+    # can be found here:
+    #
+    #    https://docs.bokeh.org/en/latest/docs/reference/core/properties.html#bokeh-core-properties
+    source=Instance(ColumnarDataSource, help="The source to draw from")
+    mapping=Dict(String, Any, help="The mapping from new columns to old columns and possibly mappers")
+    print("Import ", __implementation__)
