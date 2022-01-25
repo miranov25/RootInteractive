4dc2a9ffe51da59274f5c71bcb3b726a0de5003b
Marian Ivanov <majo.ivanov@gmail.com>
2022-01-25 16:45:41 +0100

    Fixed a typo

--- RootInteractive/InteractiveDrawing/bokeh/test_Alias.py
+++ RootInteractive/InteractiveDrawing/bokeh/test_Alias.py
@@ -173,7 +173,7 @@ def test_makeColumns():
     paramDict = {"paramA": {"value": "5"}}
     functionDict = {"saxpy": {"name": "saxpy", "fields": ["a", "x", "y"]}}
     cdsDict = {"histoA": {"nbins": 10, "type": "histogram", "variables": ["X"], "source": None}, None: {"data": df, "type": "source"}}
-    varList, ctx_updated, memoized_columns, sources = getOrMakeColumns(["1", "Y", "10*X+Y", "Y", "X*(Y**(5/2))", "X*(Y**(5/2))/Z", "sqrt(X)","paramA", "histoA.bin_count"], {}, cdsDict, paramDict, functionDict)
+    varList, ctx_updated, memoized_columns, sources = getOrMakeColumns(["1", "Y", "10*X+Y", "Y", "X*(Y**(5/2))", "X*(Y**(5/2))/Z", "sqrt(X)","paramA", "histoA.bin_count"], None, cdsDict, paramDict, functionDict)
     assert len(varList) == 9
     assert len(sources) == 6
     assert ctx_updated[-1] == "histoA"
@@ -181,5 +181,5 @@ def test_makeColumns():
     print(memoized_columns)
     print(sources)
 
-#test_makeColumns()
+test_makeColumns()
 #test_customJsFunctionBokehDrawArray_v()
