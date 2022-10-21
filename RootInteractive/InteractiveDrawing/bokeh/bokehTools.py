74704219f85df7687fda1620759ab8815a463a34
Marian Ivanov <majo.ivanov@gmail.com>
2022-10-21 09:52:32 +0200

    Fixed axis transform for X error bars

--- RootInteractive/InteractiveDrawing/bokeh/bokehTools.py
+++ RootInteractive/InteractiveDrawing/bokeh/bokehTools.py
@@ -1040,11 +1040,15 @@ def bokehDrawArray(dataFrame, query, figureArray, histogramArray=[], parameterAr
                 hover_tool_renderers[cds_name].append(drawnGlyph)
                 if variables_dict['errX'] is not None:
                     if isinstance(variables_dict['errX'], dict):
-                        errWidthX = errorBarWidthTwoSided(variables_dict['errX'], paramDict)
-                        errorX = VBar(top=varNameY, bottom=varNameY, width=errWidthX, x=varNameX, line_color=color)
+                        if x_transform:
+                            barLower, barUpper = errorBarWidthAsymmetric((variables_dict['errX'],variables_dict['errX']), variables_dict['X'], cds_used, x_transform_parsed, paramDict)
+                            errorX = Quad(top=varNameY, bottom=varNameY, left=barLower, right=barUpper, line_color=color)                          
+                        else:
+                            errWidthX = errorBarWidthTwoSided(variables_dict['errX'], paramDict)
+                            errorX = VBar(top=varNameY, bottom=varNameY, width=errWidthX, x=varNameX, line_color=color)
                     elif isinstance(variables_dict['errX'], tuple):
                         barLower, barUpper = errorBarWidthAsymmetric(variables_dict['errX'], variables_dict['X'], cds_used)
-                        errorX = Quad(top=varNameY, bottom=varNameY, left=barLower, right=barUpper, line_color=color)                        
+                        errorX = Quad(top=dataSpecY, bottom=dataSpecY, left=barLower, right=barUpper, line_color=color)                        
                     figureI.add_glyph(cds_used, errorX)
                 if variables_dict['errY'] is not None:
                     if isinstance(variables_dict['errY'], dict):
