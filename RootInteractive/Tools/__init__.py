4329c0dd58ebf20c0b53e65d87a0ac70f119715d
miranov25 <marian.ivanov@cern.ch>
2022-11-22 20:33:35 +0100

    ignore errors in the library loading

--- RootInteractive/Tools/__init__.py
+++ RootInteractive/Tools/__init__.py
@@ -1,5 +1,6 @@
 try:
     import ROOT
+    ROOT.gROOT.ProcessLine( "gErrorIgnoreLevel = 0xFFF;" ) # TODO - this is bad style - we should load library only when needed
     ROOT.gSystem.Load("$ALICE_ROOT/lib/libSTAT.so")
 
 except ImportError:
