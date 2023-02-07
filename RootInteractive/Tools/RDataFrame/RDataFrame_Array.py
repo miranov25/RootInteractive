cb7c7614cffaacb50f5a26ea34e91cde55d185c3
Marian Ivanov <majo.ivanov@gmail.com>
2023-02-07 20:46:34 +0100

    printf debugging

--- RootInteractive/Tools/RDataFrame/RDataFrame_Array.py
+++ RootInteractive/Tools/RDataFrame/RDataFrame_Array.py
@@ -340,6 +340,7 @@ class RDataFrame_Visit:
                 self.n_iter[idx] = f"{value['implementation']}.size() - {-n_iter}"
             else:
                 self.n_iter[idx] = n_iter
+        print(len(n_iter_arr), n_iter_arr)
         dtype = scalar_type(unpackScalarType(value["type"], len(n_iter_arr)))
         return {
             "implementation":f"{value['implementation']}[{sliceValue['implementation']}]",
@@ -404,7 +405,6 @@ class RDataFrame_Visit:
 
     def visit_ExtSlice(self, node:ast.ExtSlice):
         x = [self.visit_Slice(iSlice, i) for i, iSlice in enumerate(node.dims)]
-        print(str(x))
         return {"type":"int*", "implementation":']['.join([i["implementation"] for i in x]), "n_iter": [i["n_iter"] for i in x], "high_water": [i["high_water"] for i in x]}       
 
 def unpackScalarType(vecType:str, level:int=0):
