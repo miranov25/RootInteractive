c2326cf82237218a5cbc40b14c20e3f1b44521aa
Marian Ivanov <majo.ivanov@gmail.com>
2023-02-07 21:21:52 +0100

    Fixed regex fail

--- RootInteractive/Tools/RDataFrame/RDataFrame_Array.py
+++ RootInteractive/Tools/RDataFrame/RDataFrame_Array.py
@@ -411,7 +411,7 @@ class RDataFrame_Visit:
 def unpackScalarType(vecType:str, level:int=0):
     if level <= 0:
         return vecType
-    vecTypeNew = vecType.split('<',1)[1].split('>')[-2]
+    vecTypeNew = vecType.split('<',1)[1].rsplit('>',1)[-2]
     return unpackScalarType(vecTypeNew, level-1)
 
 def makeDefine(name, code, df, verbose=3, isTest=False):
