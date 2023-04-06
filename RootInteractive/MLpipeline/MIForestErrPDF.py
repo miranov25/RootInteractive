6a7a219f51b31696f2aedb2915e4f81b74847236
pl0xz0rz <majo.ivanov@gmail.com>
2023-04-06 10:51:29 +0200

    Using joblib.Parallel on median

--- RootInteractive/MLpipeline/MIForestErrPDF.py
+++ RootInteractive/MLpipeline/MIForestErrPDF.py
@@ -37,6 +37,9 @@ def _accumulate_predictionNL(predict, X, out,col):
     prediction = predict(X, check_input=False)
     out[col] += prediction
 
+def partitionBlock(allRF, k, begin, end):
+    allRF[begin:end].partition(k)
+
 def predictRFStat(rf, X, statDictionary,n_jobs):
     """
     inspired by https://github.com/scikit-learn/scikit-learn/blob/37ac6788c/sklearn/ensemble/_forest.py#L1410
@@ -58,7 +61,14 @@ def predictRFStat(rf, X, statDictionary,n_jobs):
     #
     allRFTranspose = allRF.T.copy(order='C')
     if "median" in statDictionary:
-        allRFTranspose.partition(nEstimators//2, -1)
+        blockSize = X.shape[0] // n_jobs + 1
+        block_begin = arange(0, X.shape[0], blockSize)
+        block_end = block_begin[1:]
+        block_end.append(X.shape[0])
+        Parallel(n_jobs=n_jobs, verbose=rf.verbose, require="sharedmem")(
+                delayed(partitionBlock)(allRFTranspose, nEstimators // 2, first, last)
+                for first, last in zip(block_begin, block_end)
+
         statOut["median"]= allRFTranspose[:,nEstimators//2]
     if "mean"  in statDictionary: statOut["mean"]=np.mean(allRFTranspose, -1)
     if "std"  in statDictionary: statOut["std"]=np.std(allRFTranspose, -1)
