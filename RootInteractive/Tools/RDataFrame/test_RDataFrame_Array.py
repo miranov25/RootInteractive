d2643cd2e80e28d77de11c2ebf2c841e2af24276
pl0xz0rz <majo.ivanov@gmail.com>
2023-10-04 14:01:34 +0200

    Fix for invariant test cos

--- RootInteractive/Tools/RDataFrame/test_RDataFrame_Array.py
+++ RootInteractive/Tools/RDataFrame/test_RDataFrame_Array.py
@@ -196,7 +196,7 @@ def test_define2(rdf):
     # support for the TMath:: functions
     parsed = makeDefine("array2D0_cos0", "cos(array2D0[0,:])", rdf, None, 3);         # this is working
     parsed = makeDefine("array2D0_cos1", "TMath.Cos(array2D0[0,:])", rdf, None, 3);   # this is working
-    rdf = rdf.Define("array2D0_cos_diff", "array2D0_cos0-array2D0_cos1")
+    rdf = makeDefine("array2D0_cos_diff", "array2D0_cos0-array2D0_cos1", rdf, None, 3)
     
     # support for the operator [index]
     rdf = makeDefine("array2D0_0", "array2D0[0,:]", rdf, None, 3);
