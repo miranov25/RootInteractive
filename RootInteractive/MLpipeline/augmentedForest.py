93e85d21a7184881cc69c32fb5874243b142ddec
Marian Ivanov <majo.ivanov@gmail.com>
2024-11-19 09:08:03 +0100

    Add second approach

--- RootInteractive/MLpipeline/augmentedForest.py
+++ RootInteractive/MLpipeline/augmentedForest.py
@@ -2,6 +2,7 @@ import numpy as np
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.tree import DecisionTreeRegressor
 from joblib import Parallel, delayed
+from RootInteractive.MLpipeline.MIForestErrPDF import predictRFStat
 
 class AugmentedForestRegressor(RandomForestRegressor):
     def __init__(self,
@@ -40,14 +41,40 @@ class AugmentedForestRegressor(RandomForestRegressor):
         self.noise_levels = noise_levels
 
     def _fit_tree(self, tree, X, y, sample_weight, noise_levels):
-        noise = np.random.normal(0,noise_levels)
-        tree.fit(X,y+noise,sample_weight)
+        tree.fit(X,y,sample_weight)
         return tree
 
     def fit(self, X, y, sample_weight=None):
         '''
         Override 
         '''
+        total_samples = X.shape[0]
+        indices = np.random.permutation(total_samples)
+        subset_size = total_samples // self.n_estimators
+        block_begin = range(0, total_samples, subset_size)
         parallel_runner = Parallel(n_jobs=self.n_jobs, verbose=self.verbose)
-        parallel_runner(delayed(self._fit_tree)(self._make_estimator(), X, y, sample_weight=sample_weight, noise_levels=self.noise_levels))
+        parallel_runner(
+            delayed(self._fit_tree)(
+                self._make_estimator(), X, y, sample_weight=sample_weight, noise_levels=self.noise_levels) for x in block_begin
+            )
         return self
+
+class AugmentedRandomForestArray:
+    # Array of N standard random forests
+    # Input parameters include standard options like kernel width
+    def __init__(self, n_forests, n_repetitions, n_jobs):
+        self.n_jobs = n_jobs
+
+    def fit(self, X, Y, sigma, sample_weight=None):
+        pass
+        # Fit each random forest with augmented input data
+
+    def predict(self):
+        flattened_forest = [estimator for forest in self.estimators for estimator in forest]
+        pass
+        # Use an ensemble of forests for prediction
+        # Unlike standard RFStat which uses a single array of trees, this method uses an array of arrays
+
+    def predictRFStat(self, X, statDictionary, n_jobs):
+        pass
+        # Definition for detailed statistical prediction using random forests
