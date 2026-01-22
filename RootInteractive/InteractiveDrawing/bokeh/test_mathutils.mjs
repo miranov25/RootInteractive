748ae758d7bbe8b4c874a8aa2c6a2fa051b2ede0
Marian Ivanov <majo.ivanov@gmail.com>
2026-01-22 09:12:27 +0100

    Fixed JS test

--- RootInteractive/InteractiveDrawing/bokeh/test_mathutils.mjs
+++ RootInteractive/InteractiveDrawing/bokeh/test_mathutils.mjs
@@ -6,36 +6,43 @@ function shallow_compare_absolute(A,B,delta){
 	return A.reduce((acc,cur,idx)=>(acc*idx+Math.abs(cur-B[idx]))/(idx+1), 0) <= delta
 }
 
+function assert(cond, msg) {
+  if (!cond) {
+    throw new Error(msg);
+  }
+}
+
 function test_chol(){
 	// Identity matrix
 	let A = [1,0,1,0,0,1]
 	let A_llt = [1,0,1,0,0,1]
 	MathUtils.chol(A,3)
-	console.assert(shallow_compare_absolute(A,A_llt,1e-6), "expected "+A_llt+" got "+A)	
-
+	assert(shallow_compare_absolute(A,A_llt,1e-6), "expected "+A_llt+" got "+A)	
 }
 
 function test_unbinned_quantile(){
 	let A = [1,2,3,4,5,6,7,8,9]
 
 	let q = MathUtils.quantile(A,0.5)
-	console.assert(q === 5, "expected 5 got "+q)
+	assert(q === 5, "expected 5 got "+q)
 
 	q = MathUtils.quantile(A, 0.25, 0, -1)
-	console.assert(q === 3, "expected 3 got "+q)
+	assert(q === 3, "expected 3 got "+q)
 
 	q = MathUtils.quantile(A,0.75, 0, -1)
-	console.assert(q === 7, "expected 7 got "+q)
+	assert(q === 7, "expected 7 got "+q)
 
 	q = MathUtils.quantile(A,0, 0, -1)
-	console.assert(q === 1, "expected 1 got "+q)
+	assert(q === 1, "expected 1 got "+q)
 
 	q = MathUtils.quantile(A,1, 0, -1)
-	console.assert(q === 9, "expected 9 got "+q)
+	assert(q === 9, "expected 9 got "+q)
 
 	q = MathUtils.quantile(A,0.0625, 0, -1)
-	console.assert(q === 1.5, "expected 1.5 got "+q)
+	assert(q === 1.5, "expected 1.5 got "+q)
 }
 
 test_chol()
 test_unbinned_quantile()
+
+throw new Error("All tests passed")
