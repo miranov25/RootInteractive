00db1619adb0cdc014039d411ea6b5576915e022
Marian Ivanov <majo.ivanov@gmail.com>
2026-02-06 15:40:53 +0100

    Added invariance test for sqrt scaling rounding

--- RootInteractive/InteractiveDrawing/bokeh/test_compression_integration.py
+++ RootInteractive/InteractiveDrawing/bokeh/test_compression_integration.py
@@ -162,8 +162,46 @@ def test_compression_delta():
     run_test_in_node(data_exported, temp_dir)
 
 
+@pytest.mark.feature("ENC.base64.float64")
+@pytest.mark.feature("ENC.base64.int32")
+@pytest.mark.feature("ENC.compression.zip")
+@pytest.mark.feature("ENC.compression.roundtrip")
+@pytest.mark.feature("ENC.compression.sinh")
+@pytest.mark.backend("node")
+@pytest.mark.layer("invariance")
+def test_compression_sinh():
+    temp_dir = tempfile.gettempdir()
+    cwd = pathlib.Path(__file__).parent.resolve()
+    subprocess.run(["node", "node_modules/typescript/bin/tsc",
+                    '--module', 'None',
+                    '--target', 'ES2020',
+                    '--outDir', str(temp_dir),
+                    f'{cwd}/SerializationUtils.ts'], check=True, cwd=cwd) 
+    
+    sigma0 = 1e-3
+    sigma1 = 10
+    arrayCompression = [("sqrt_scaling",sigma0,sigma1), "zip", "base64"]
+    compressed = {i: compressArray(testcase, arrayCompression) for i, testcase in TEST_CASES.items()}
+    data_exported = [{
+        "meta":{
+            "name": i,
+            "enableDithering": False,
+            "byteorder": sys.byteorder,
+            "dtype": dtypes[i],
+            "length": lengths[i],
+            "abs_tol": .5 * sigma0 * sigma0 * sigma1,
+            "rel_tol": .5 * sigma0,
+            "nan_equal": True
+        },
+        "pipeline": compressed[i]["history"],
+        "data_compressed": compressed[i]["array"],
+        "data_ref": test_b64[i]
+    } for i, testcase in TEST_CASES.items()]    
+    run_test_in_node(data_exported, temp_dir)
+
+
 if __name__ == "__main__":
     #test_serializationutils()
     #test_compression_simple()
     #test_compression_relative16()
-    test_compression_delta()
+    test_compression_sinh()
